efb4cfe643768a176b3f9006384dda4735e8c53b
Sarthak Consul <32162912+SConsul@users.noreply.github.com>
2018-04-21 18:05:17 +0530

    Update slowto1000.vhd

--- Reaction_Game/slowto1000.vhd
+++ Reaction_Game/slowto1000.vhd
@@ -1,29 +1,62 @@
+library std ;
+use std.standard.all;
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity slowto1000 is
-  port (reset, CLK_50M: in std_logic; CLK_1k: out std_logic);
+port(reset, fast_clk : in std_logic; 
+		slow_clk : out std_logic);
 end entity slowto1000;
+architecture behave of slowto1000 is
 
-
-architecture slow of slowto1000 is
-
-component slow16 is
-  port (reset, fast_clk: in std_logic; slow_clk: out std_logic);
-end component;
-
-component slow5 is
-		port (reset, fast_clk: in std_logic; slow_clk: out std_logic);
-	end component;
-	
-signal s1, s2, s3, s4, s5 : std_logic;
-begin 
---Need 1 slow16, 5 slow5 to convert50Mhx to 128Hz
-sl1: slow5 port map(reset=>reset, fast_clk=>CLK_50M, slow_clk=>s1);
-sl2: slow5 port map(reset=>reset, fast_clk=>s1,  slow_clk=>s2);
-sl3: slow5 port map(reset=>reset, fast_clk=>s2,  slow_clk=>s3);
-sl4: slow5 port map(reset=>reset, fast_clk=>s3,  slow_clk=>s4);
-sl5: slow5 port map(reset=>reset, fast_clk=>s4,  slow_clk=>s5);
-sl6: slow16 port map(reset=>reset, fast_clk=>s5,  slow_clk=>CLK_1k);
-
-end slow;
+	type sl_RTLState is (zero, one);
+	signal sl_rtl_state: sl_RTLState;
+	signal slow_clk_sig : std_logic;
+	signal sl_count: unsigned (14 downto 0);
+	constant nill: unsigned (14 downto 0):= (others => '0') ;
+		
+begin
+	slow_clk<=slow_clk_sig;
+	process(reset,fast_clk, slow_clk_sig, sl_rtl_state, sl_count)
+		variable next_sl_rtl_state:sl_RTLState;
+		variable sl_count_var: unsigned (14 downto 0);
+		variable slow_clk_var:std_logic;
+		--25k = 0b110000110101000
+		begin
+			slow_clk_var:=slow_clk_sig;
+			next_sl_rtl_state:=sl_rtl_state;
+			sl_count_var:=sl_count;
+			
+			case sl_rtl_state is
+				when zero=>
+					slow_clk_var:='0';
+					if(sl_count_var=24999) then 
+						next_sl_rtl_state:=one;
+						sl_count_var:=nill;
+					else
+						sl_count_var:=sl_count_var+1;
+					end if;
+					
+				when one=>
+					slow_clk_var:='1';
+					if(sl_count_var=24999) then
+						next_sl_rtl_state:=zero;
+						sl_count_var:=nill;
+					else
+						sl_count_var:=sl_count_var+1;
+					end if;
+			end case;
+			
+			if(rising_edge(fast_clk)) then
+				if(reset='1') then
+					sl_rtl_state<=zero;
+					sl_count<=nill;
+				else
+					sl_rtl_state<=next_sl_rtl_state;
+					sl_count<=sl_count_var;
+					slow_clk_sig<= slow_clk_var;
+			end if;
+		end if;
+	end process;
+end behave;
